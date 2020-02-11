1053e24fef3ec1f3c8ef9495c76c4c8122927a61
Daniel Hopp <daniel.hopp@metr.systems>
2020-02-11 16:51:35 +0100

    Fixes sorting arrays in calc_auc function

--- src/evaluation/roc_auc.jl
+++ src/evaluation/roc_auc.jl
@@ -2,6 +2,61 @@ using Plots
 
 export plot_auc
 
+function calc_auc(xs::Array, ys::Array)
+    sorted = sort([(i, j) for (i, j) in zip(xs, 1:length(xs))])
+    index = [j for (i, j) in sorted]
+    sorted_x = [i for (i, j) in sorted]
+    sorted_y = ys[index]
+    auc = 0
+    for i = 2:length(sorted_x)
+        # rectangular component
+        x = sorted_x[i] - sorted_x[i-1]
+        y = sorted_y[i-1]
+        auc += x * y
+        # triangular component
+        height = sorted_y[i] - y
+        auc += 0.5 * x * height
+    end
+    return auc
+end
+
+prediction(probability_positive::Array{Float64}, threshold::Float64) = [x > threshold ? 1 : 0 for x in probability_positive]
+
+"""
+returns true positive and false positive rates given arrays of actuals and predictions
+"""
+function tp_fp(actual::Array{Int64}, pred::Array{Int64})
+    tpr = sum([if pred[i] == 1 && actual[i] == 1
+        1
+    else
+        0
+    end for i in collect(range(1, stop = length(pred)))]) / sum(actual)
+    fp = sum([if pred[i] == 1 && actual[i] == 0
+        1
+    else
+        0
+    end for i in collect(range(1, stop = length(pred)))])
+    tn = sum([if pred[i] == 0 && actual[i] == 0
+        1
+    else
+        0
+    end for i in collect(range(1, stop = length(pred)))])
+    fpr = fp / (fp + tn)
+    return tpr, fpr
+end
+
+function roc_data(actual::Array{Int64}, probability_positive::Array{Float64}, step = 0.01)
+    tps = []
+    fps = []
+    for i in range(0, 1, step = step)
+        tp, fp = tp_fp(actual, prediction(probability_positive, i))
+        push!(tps, tp)
+        push!(fps, fp)
+    end
+    auc = calc_auc(fps, tps)
+    return tps, fps, auc
+end
+
 """
 _given array of predictions (0 ≤ x ≤ 1] and array of actuals, return ROC AUC plot_
 
@@ -15,59 +70,6 @@ _given array of predictions (0 ≤ x ≤ 1] and array of actuals, return ROC AUC
     plot of ROC AUC
 """
 function plot_auc(actual::Array{Int}, pred::Array{Float64})
-    function calc_auc(xs::Array, ys::Array)
-        sorted_x = sort(xs, by=j->Dict(Pair.(xs,ys))[j])
-        sorted_y = sort(ys, by=j->Dict(Pair.(xs,ys))[j])
-        auc = 0
-        for i = 2:length(sorted_x)
-            # rectangular component
-            x = sorted_x[i] - sorted_x[i-1]
-            y = sorted_y[i-1]
-            auc += x * y
-            # triangular component
-            height = sorted_y[i] - y
-            auc += 0.5 * x * height
-        end
-        return auc
-    end
-    function prediction(probability_positive, threshold)
-        [if x > threshold
-            1
-        else
-            0
-        end for x in probability_positive]
-    end
-    function tp_fp(pred, actual)
-        tpr = sum([if pred[i] == 1 && actual[i] == 1
-            1
-        else
-            0
-        end for i in collect(range(1, stop = length(pred)))]) / sum(actual)
-        fp = sum([if pred[i] == 1 && actual[i] == 0
-            1
-        else
-            0
-        end for i in collect(range(1, stop = length(pred)))])
-        tn = sum([if pred[i] == 0 && actual[i] == 0
-            1
-        else
-            0
-        end for i in collect(range(1, stop = length(pred)))])
-        fpr = fp / (fp + tn)
-        return tpr, fpr
-    end
-    function roc_data(probability_positive, actual, step = 0.01)
-        tps = []
-        fps = []
-        for i in range(0, 1, step = step)
-            tp, fp = tp_fp(prediction(probability_positive, i), actual)
-            push!(tps, tp)
-            push!(fps, fp)
-        end
-        auc = calc_auc(fps, tps)
-        return tps, fps, auc
-    end
-
     # diagonal line
     p1 = plot(
         [0, 1],
@@ -78,7 +80,7 @@ function plot_auc(actual::Array{Int}, pred::Array{Float64})
         ylim = (0, 1),
         xlim = (0, 1),
     )
-    y, x, auc = roc_data(pred, actual)
+    y, x, auc = roc_data(actual, pred)
     plot!(x, y, label = "AUC=$(round(auc, digits=4))", color = :blue)
 
     plot!(legend = :bottomright)
