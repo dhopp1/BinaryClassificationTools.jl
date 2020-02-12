4eb9960bb2f0d817469f4b9e9c39fe8aa69521c6
Daniel Hopp <daniel.hopp@metr.systems>
2020-02-12 12:38:38 +0100

    Adds test for confusion matrix ratio option

--- test/evaluation/test_diagnostics.jl
+++ test/evaluation/test_diagnostics.jl
@@ -23,6 +23,7 @@ data_proba = [0.9, 0.8, 0.52, 0.2, 0.55, 0.52, 0.6, 0.1, 0.45, 0.3, 0.8]
     @test round(accuracy(data_actual, data_pred), digits=4) == 0.5455
     @test round(f1_score(data_actual, data_pred), digits=4) == 0.5333
     @test confusion_matrix(data_actual, data_pred) == [4.0 3.0; 2.0 2.0]
+    @test round.(confusion_matrix(data_actual, data_pred, ratio=true), digits=4) == [0.3636 0.2727; 0.1818 0.1818]
     @test predict_from_threshold(data_proba, 0.5) == data_pred
 
     @test best_f1_search(data_actual, data_proba)[1:2] == (0.2, 0.8)
