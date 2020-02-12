ddd2f40699e892380bf1584650a861c4000852f7
Daniel Hopp <daniel.hopp@metr.systems>
2020-02-12 10:42:18 +0100

    Edits main .jl to reflect refactoring

--- src/BinaryClassificationTools.jl
+++ src/BinaryClassificationTools.jl
@@ -1,7 +1,6 @@
 module BinaryClassificationTools
 
-include("evaluation/confusion_matrix.jl")
-include("evaluation/f1.jl")
+include("evaluation/diagnostics.jl")
 include("evaluation/roc_auc.jl")
 
 end
